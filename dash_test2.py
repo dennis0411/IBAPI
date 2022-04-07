cd34dcb014d0a2e054f07813c485a0469ffe9e31
dennis0411 <dennis810411@gmail.com>
2022-04-07 16:47:21 +0800

    dash table

---
+++ dash_test2.py
@@ -0,0 +1,235 @@
+import dash
+from dash import dcc
+from dash import Dash, html, dash_table
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output
+from pymongo import MongoClient
+from warnings import simplefilter
+import numpy as np
+import pandas as pd
+from plotly.colors import n_colors
+
+import plotly.express as px
+import datetime
+from datetime import date
+from plotly.subplots import make_subplots
+
+# 列印用
+desired_width = 320
+pd.set_option('display.width', desired_width)
+np.set_printoptions(linewidth=desired_width)
+pd.set_option('display.max_columns', 10)
+
+# 取消 future warning
+simplefilter(action='ignore', category=FutureWarning)
+
+# 引入密碼
+path = "mongodb_password"
+with open(path) as f:
+    word = f.readline().split(',')
+    account = word[0]
+    password = word[1]
+
+
+# Build Function
+def read_AccountSummary(download_date):
+    df = collection.find({'$and': [{'date': download_date}, {'tag': "AccountSummary"}]})
+    AccountSummary = pd.DataFrame()
+    for account_data in df:
+        data = pd.DataFrame(account_data['data'])
+        AccountSummary = pd.concat([AccountSummary, data], axis=0, ignore_index=True)
+    return AccountSummary
+
+
+def read_Portfolio(download_date):
+    df = collection.find({'$and': [{'date': download_date}, {'tag': "Portfolio"}]})
+    Portfolio = pd.DataFrame()
+    for account_data in df:
+        data = pd.DataFrame(account_data['data'])
+        Portfolio = pd.concat([Portfolio, data], axis=0, ignore_index=True)
+    return Portfolio
+
+def ib_data_process(download_date):
+    AccountSummary = read_AccountSummary(download_date).pivot(index="Account", columns="tag",
+                                                              values='value')
+
+    Portfolio = read_Portfolio(download_date)
+
+    # 帳戶資料新增持倉
+    for account in AccountSummary.index:
+        secType_filt = {"StockValue": (Portfolio["Account"] == account) & (Portfolio["secType"] == "STK"),
+                        "BondValue": (Portfolio["Account"] == account) & (Portfolio["secType"] == "BOND"),
+                        "OPTValue": (Portfolio["Account"] == account) & (Portfolio["secType"] == "OPT"),
+                        "FUTValue": (Portfolio["Account"] == account) & (Portfolio["secType"] == "FUT"),
+                        }
+        for filt in secType_filt.keys():
+            AccountSummary.loc[account, filt] = Portfolio[secType_filt.get(filt)]['marketValue'].sum()
+
+    # 投資組合資產分類
+    Portfolio_STK = Portfolio[Portfolio["secType"] == "STK"][[
+        'Account', 'symbol', 'position', 'marketPrice', 'marketValue', 'averageCost', 'unrealizedPNL']]
+    Portfolio_BOND = Portfolio[Portfolio["secType"] == "BOND"][[
+        'Account', 'symbol', 'lastTradeDate', 'position', 'marketPrice', 'marketValue', 'averageCost', 'unrealizedPNL']]
+    Portfolio_OPT = Portfolio[Portfolio["secType"] == "OPT"][[
+        'Account', 'symbol', 'right', 'strike', 'lastTradeDate', 'position', 'marketPrice', 'marketValue',
+        'averageCost', 'unrealizedPNL']]
+    Portfolio_FUT = Portfolio[Portfolio["secType"] == "FUT"][[
+        'Account', 'symbol', 'lastTradeDate', 'position', 'marketPrice', 'marketValue', 'averageCost', 'unrealizedPNL']]
+
+    data_dict = {"AccountSummary": AccountSummary,
+                 "Portfolio_STK": Portfolio_STK,
+                 "Portfolio_BOND": Portfolio_BOND,
+                 "Portfolio_OPT": Portfolio_OPT,
+                 "Portfolio_FUT": Portfolio_FUT}
+    return data_dict
+
+
+
+# mongodb connection
+CONNECTION_STRING = f"mongodb+srv://{account}:{password}@getdata.dzc20.mongodb.net/getdata?retryWrites=true&w=majority"
+client = MongoClient(CONNECTION_STRING, tls=True, tlsAllowInvalidCertificates=True)
+db = client.getdata
+collection = db.ib
+
+download_date = '2022/04/06'
+data = ib_data_process(download_date)
+AccountSummary = data["AccountSummary"].reset_index()
+Portfolio_BOND = data["Portfolio_BOND"].reset_index()
+
+# make app
+app = dash.Dash()
+
+app.layout = html.Div(
+    className="row",
+    children=[
+        html.Div(
+            dash_table.DataTable(
+                id='table-paging-with-graph',
+                columns=[
+                    {"name": i, "id": i} for i in sorted(AccountSummary.columns)
+                ],
+                page_current=0,
+                page_size=50,
+                page_action='custom',
+
+                filter_action='custom',
+                filter_query='',
+
+                sort_action='custom',
+                sort_mode='multi',
+                sort_by=[]
+            ),
+            style={'height': 750, 'overflowY': 'scroll'},
+            className='six columns'
+        ),
+        html.Div(
+            id='table-paging-with-graph-container',
+            className="five columns"
+        )
+    ]
+)
+
+operators = [['ge ', '>='],
+             ['le ', '<='],
+             ['lt ', '<'],
+             ['gt ', '>'],
+             ['ne ', '!='],
+             ['eq ', '='],
+             ['contains '],
+             ['datestartswith ']]
+
+
+def split_filter_part(filter_part):
+    for operator_type in operators:
+        for operator in operator_type:
+            if operator in filter_part:
+                name_part, value_part = filter_part.split(operator, 1)
+                name = name_part[name_part.find('{') + 1: name_part.rfind('}')]
+
+                value_part = value_part.strip()
+                v0 = value_part[0]
+                if (v0 == value_part[-1] and v0 in ("'", '"', '`')):
+                    value = value_part[1: -1].replace('\\' + v0, v0)
+                else:
+                    try:
+                        value = float(value_part)
+                    except ValueError:
+                        value = value_part
+
+                # word operators need spaces after them in the filter string,
+                # but we don't want these later
+                return name, operator_type[0].strip(), value
+
+    return [None] * 3
+
+
+@app.callback(
+    Output('table-paging-with-graph', "data"),
+    Input('table-paging-with-graph', "page_current"),
+    Input('table-paging-with-graph', "page_size"),
+    Input('table-paging-with-graph', "sort_by"),
+    Input('table-paging-with-graph', "filter_query"))
+def update_table(page_current, page_size, sort_by, filter):
+    filtering_expressions = filter.split(' && ')
+    dff = AccountSummary
+    for filter_part in filtering_expressions:
+        col_name, operator, filter_value = split_filter_part(filter_part)
+
+        if operator in ('eq', 'ne', 'lt', 'le', 'gt', 'ge'):
+            # these operators match pandas series operator method names
+            dff = dff.loc[getattr(dff[col_name], operator)(filter_value)]
+        elif operator == 'contains':
+            dff = dff.loc[dff[col_name].str.contains(filter_value)]
+        elif operator == 'datestartswith':
+            # this is a simplification of the front-end filtering logic,
+            # only works with complete fields in standard format
+            dff = dff.loc[dff[col_name].str.startswith(filter_value)]
+
+    if len(sort_by):
+        dff = dff.sort_values(
+            [col['column_id'] for col in sort_by],
+            ascending=[
+                col['direction'] == 'asc'
+                for col in sort_by
+            ],
+            inplace=False
+        )
+
+    return dff.iloc[
+        page_current*page_size: (page_current + 1)*page_size
+    ].to_dict('records')
+
+
+@app.callback(
+    Output('table-paging-with-graph-container', "children"),
+    Input('table-paging-with-graph', "data"))
+def update_graph(rows):
+    dff = pd.DataFrame(rows)
+    return html.Div(
+        [
+            dcc.Graph(
+                id=column,
+                figure={
+                    "data": [
+                        {
+                            "x": dff["Account"],
+                            "y": dff[column] if column in dff else [],
+                            "type": "bar",
+                            "marker": {"color": "#0074D9"},
+                        }
+                    ],
+                    "layout": {
+                        "xaxis": {"automargin": True},
+                        "yaxis": {"automargin": True},
+                        "height": 250,
+                        "margin": {"t": 10, "l": 10, "r": 10},
+                    },
+                },
+            )
+            for column in ["NetLiquidation", "TotalCashValue", "StockValue", "BondValue"]
+        ]
+    )
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
